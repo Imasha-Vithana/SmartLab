e3f03647716654a2a4e200aef6b55fd578dfaec7
Imasha Vithana <imashahashini955@gmail.com>
2026-09-18 01:03:39 -0700

    Connected application to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const dotnev = require("dotenv");
 require("dotenv").config();
 
 const app = express();
 
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+
+// Test Route
 app.get("/", (req, res) => {
     res.send("SmartLab API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// MongoDB Connection
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+        family: 4,
+        serverSelectionTimeoutMS: 5000
+    })
+    .then(() => {
+        console.log("MongoDB connected successfully.");
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:");
+        console.error(error.message);
+    });
