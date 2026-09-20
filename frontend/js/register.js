b5ce4f8a0807157df0189ce1284b907c991cc355
Imasha Vithana <imashahashini955@gmail.com>
2026-09-20 14:11:14 -0700

    Fix double slash in fetch URL

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -24,7 +24,7 @@ registerForm.addEventListener(
         try {
 
             const response = await fetch(
-                "https://smartlab-production-5fe7.up.railway.app//api/auth/register", {
+                "https://smartlab-production-5fe7.up.railway.app/api/auth/register", {
                     method: "POST",
 
                     headers: {
